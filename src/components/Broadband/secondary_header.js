a503eff9a08d8854c34324194a97360d5f8eed03
Wayne Studley <waynes.studley@moneyexpert.com>
2020-03-06 13:27:24 +0000

    remove reference to supercard

--- src/components/Broadband/secondary_header.js
+++ src/components/Broadband/secondary_header.js
@@ -43,9 +43,6 @@ class SecondaryHeader extends Component {
                             { (!this.props.globalState.isBtJourney || this.props.isShowNewQuote === true) && !this.props.globalState.isMultiJourney && 
                                 <a href='#/start' onClick={this.resetSession}>New&nbsp;Quote</a>
                             }
-                            {this.props.globalState.isSupercard &&
-                                <a href='#'  onClick={(e) => openModal(e, 'modalSuperCard')}>Free&nbsp;SuperCard</a>
-                            }
                         </div>
                         <div className='top-button-wrapper'>
                             {!this.props.globalState.isWeb &&
@@ -83,42 +80,6 @@ class SecondaryHeader extends Component {
                         </div>
                     </header>
                 </div>
-
-                {/*modal window*/}
-                <div id='modalSuperCard' className='modal-window'>
-                    <a href='#' className='background-close' onClick={(e) => closeModal(e, 'modalSuperCard')} />
-                    <div>
-                        <a href='#' title='CloseButton' className='modal-close' onClick={(e) => closeModal(e, 'modalSuperCard')} />
-                        <div className=''>
-                            <div className='supercard-wrapper'>
-                                <h2><strong>Free SuperCard</strong> - worth over &pound;2000 when you switch with us today</h2>
-                                <p>With 12 months free Supercard membership, you can enjoy discounts and genuine freebies from some of the UK’s best-known brands.</p>
-                                <div className='brand-logos-wrapper'>
-                                    <img src={process.env.PUBLIC_URL + './imagesPackage/Supercard/alton-towers.jpg'} alt='alton towers' />
-                                    <img src={process.env.PUBLIC_URL + './imagesPackage/Supercard/argos.jpg'} alt='argos' />
-                                    <img src={process.env.PUBLIC_URL + './imagesPackage/Supercard/currys.jpg'} alt='currys' />
-                                    <img src={process.env.PUBLIC_URL + './imagesPackage/Supercard/gap.jpg'} alt='gap' />
-                                    <img src={process.env.PUBLIC_URL + './imagesPackage/Supercard/halfords.jpg'} alt='halfords' />
-                                    <img src={process.env.PUBLIC_URL + './imagesPackage/Supercard/house-of-fraser.jpg'} alt='house of fraser' />
-                                    <img src={process.env.PUBLIC_URL + './imagesPackage/Supercard/merlin-entertainment.jpg'} alt='merlin entertainment' />
-                                    <img src={process.env.PUBLIC_URL + './imagesPackage/Supercard/pizza-express.jpg'} alt='pizza express' />
-                                    <img src={process.env.PUBLIC_URL + './imagesPackage/Supercard/starbucks.jpg'} alt='starbucks' />
-                                    <img src={process.env.PUBLIC_URL + './imagesPackage/Supercard/zizzi.jpg'} alt='zizzi' />
-                                </div>
-                                <div className='supercard-list-wrapper'>
-                                    <ul>
-                                        <li>Free 12 month Dining card - Gourmet Society</li>
-                                        <li>4% discount at Tesco</li>
-                                        <li>Up to 40% off cinema tickets at Vue, Odeon, Showcase and Empire</li>
-                                        <li>Free and discounted pamper treatments</li>
-                                        <li>12 months free ZugarZnap gadget insurance up to &pound;1,000</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {/*end modal window*/}
             </div>
         );
     }
